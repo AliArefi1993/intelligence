8152d81304f5d63dbb77007c14043f0a2dcf28ce
…A <a@As-MacBook-Air.local>
2022-06-04 10:47:22 +0430

    dockerise project

---
+++ app/api.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import redis
+
+app = FastAPI()
+
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc
+           if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.get("/")
+async def root():
+    count = get_hit_count()
+    return {"message": "Hello World", "count": count}
---
+++ bot/main.py
@@ -0,0 +1,9 @@
+def main():
+    user_message = input('')
+    if user_message in ['Hello', 'hello', 'hi', 'Hi']:
+        print('hello Dear')
+    else:
+        print('incomprehensible')
+
+
+main()
